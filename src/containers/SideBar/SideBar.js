0b1f25c2c0f3425fca2538b58e5328765e162683
Huong Ngo <huong.ngo2@myy.haaga-helia.fi>
2019-01-25 15:12:11 +0200

    sidebar footer component

--- src/containers/SideBar/SideBar.js
+++ src/containers/SideBar/SideBar.js
@@ -2,6 +2,8 @@ import React from "react";
 import SideBarItem from "./SideBarItem/SideBarItem";
 import { Menu, Divider } from "semantic-ui-react";
 import SideBarHeader from "./SideBarHeader/SideBarHeader";
+import Subscriptions from "./Subscriptions/Subscriptions";
+import SideBarFooter from "./SideBarFooter/SideBarFooter";
 import "./SideBar.scss";
 
 class SideBar extends React.Component {
@@ -17,6 +19,7 @@ class SideBar extends React.Component {
         <SideBarItem label="Watch later" icon="clock" />
         <SideBarItem label="Liked videos" icon="thumbs up" />
         <Divider />
+        <Subscriptions />
         <SideBarHeader title="More from Youtube" />
         <SideBarItem label="Movies and Shows" icon="film" />
         <Divider />
@@ -24,6 +27,7 @@ class SideBar extends React.Component {
         <SideBarItem label="Help" icon="help circle" />
         <SideBarItem label="Send feedback" icon="comment" />
         <Divider />
+        <SideBarFooter />
       </Menu>
     );
   }
