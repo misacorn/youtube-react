8a0f07d0937894d99c87df27de8a9f7f300e6574
Huong Ngo <huong.ngo2@myy.haaga-helia.fi>
2019-01-26 16:38:44 +0200

    component to preview the video

--- src/containers/SideBar/SideBarHeader/SideBarHeader.js
+++ src/containers/SideBar/SideBarHeader/SideBarHeader.js
@@ -3,7 +3,7 @@ import "./SideBarHeader.scss";
 import { Menu } from "semantic-ui-react";
 
 const SideBarHeader = props => {
-  const heading = props.title ? props.title.toUpperCase() : " ";
+  const heading = props.title ? props.title.toUpperCase() : "";
   return (
     <Menu.Item>
       <Menu.Header className="side-bar-header">{heading}</Menu.Header>
