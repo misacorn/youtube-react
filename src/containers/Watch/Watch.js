fcd498fb127b87e71d5bc3ef3f376cf9c9a2b3ad
Huong Ngo <huong.ngo2@myy.haaga-helia.fi>
2019-01-30 12:56:00 +0200

    add Video Preview in watch Page

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -1,13 +1,18 @@
-import React, { Component } from "react";
+import React, { Fragment, Component } from "react";
 import Video from "../../components/Video/Video";
+import VideoPreview from "../../components/VideoPreview/VideoPreview";
 import "./Watch.scss";
 
 class Watch extends Component {
   render() {
     return (
-      <div style={{ maxWidth: "80%" }}>
-        <Video id="TdrL3QxjyVw" />
-      </div>
+      <Fragment>
+        <div style={{ maxWidth: "60%" }}>
+          <Video id="TdrL3QxjyVw" />
+        </div>
+        <VideoPreview horizontal={true} />
+        <VideoPreview />
+      </Fragment>
     );
   }
 }
